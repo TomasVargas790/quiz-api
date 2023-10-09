59ef2e1954a3f11a8ee1866479e70097601baec4
Tomi <vargastomas2003@gmail.com>
2023-10-09 19:28:23 -0300

    imports failed

--- api/routes.js
+++ api/routes.js
@@ -1,8 +1,8 @@
 import { Router } from 'express';
-import answers from './components/answers/network';
-import questions from './components/questions/network';
-import descriptions from './components/descriptions/network';
-import themes from './components/themes/network';
+import answers from './components/answers/network.js';
+import questions from './components/questions/network.js';
+import descriptions from './components/descriptions/network.js';
+import themes from './components/themes/network.js';
 
 const router = Router();
 
